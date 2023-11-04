3a95d07051763b1e33c154962661a5fead67fb26
Janet Webster <twixmixy@gmail.com>
2023-11-04 11:49:02 -0400

    Created BlogPost model

---
+++ models/BlogPost.js
@@ -0,0 +1,44 @@
+// import
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.INTEGER,
+        },
+        date_created: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blogPost',
+    }
+);
+
+module.exports = BlogPost;
