18c4809e186427f91d67335c3e7e07f2c36aa1ac
Janet Webster <twixmixy@gmail.com>
2023-11-04 16:22:42 -0400

    delete blog post javascript function

---
+++ public/js/deleteBlogPost.js
@@ -0,0 +1,24 @@
+const deletePost = async (event) => {
+    event.preventDefault();
+    console.log("clicked delete post");
+    console.log(event.target);
+
+    let blogPost = window.location.pathname.split('/');
+    console.log(blogPost);
+
+    const response = await fetch(`/api/blogPost/${blogPost[2]}`, {
+        method: "DELETE",
+    });
+
+    if (response.ok) {
+        document.location.assign('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+const deleteButton = document.querySelectorAll("#deleteBtn");
+
+for (let i = 0; i < deleteButton.length; i++) {
+    deleteButton[i].addEventListener("click", deletePost);
+}
