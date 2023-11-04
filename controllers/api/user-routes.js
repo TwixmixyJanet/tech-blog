d5b6088f221da0fdf04cd36bfcf9a9f96c858e1c
Janet Webster <twixmixy@gmail.com>
2023-11-04 13:12:20 -0400

    Built out api use routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,60 @@
+// imports 
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body } });
+
+        if (!userData) {
+            console.log("Could not find that user");
+            res.status(400).json({ message: "Incorrect email. Please try again."});
+            return;
+        };
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            console.log("That password did not match");
+            res.status(400).json({ message: "Incorrect password. Please try again."});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({ user: userData, message: "You have successfully logged in" });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+// exports
+module.exports = router;
