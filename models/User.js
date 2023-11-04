468a81f1ac965661341eb30cfa91e873cc9d5074
Janet Webster <twixmixy@gmail.com>
2023-11-04 11:33:56 -0400

    Coding out User

---
+++ models/User.js
@@ -0,0 +1,25 @@
+// imports
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    }
+)
