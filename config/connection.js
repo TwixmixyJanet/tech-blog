27f9c231bdc77a58375594501ca4e6b6675c7681
Janet Webster <twixmixy@gmail.com>
2023-11-04 10:16:31 -0400

    Built the connections file code

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+// imports
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+// initiating environment
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host:'127.0.0.1', // change to 'localhost'
+        dialect: 'mysql',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    });
+
+// export
+module.exports = sequelize;
