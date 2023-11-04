31d78fdaaea6340f6be256d49581f34fc05ceb7c
Janet Webster <twixmixy@gmail.com>
2023-11-04 12:38:59 -0400

    home routes for create and login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -53,6 +53,7 @@ router.get('/blogPost/:id', withAuth, async (req, res) => {
             logged_in: req.session.logged_in,
         });
     } catch (err) {
+        console.log(err)
         res.status(500).json(err);
         res.redirect('/login');
     }
@@ -81,6 +82,70 @@ router.get('/dashboard', withAuth, async (req, res) => {
             logged_in: true,
         });
     } catch (err) {
+        console.log(err)
         res.status(500).json(err);
     }
 });
+
+router.get('/create', async (req, res) => {
+    try {
+        if (req.session.logged_in) {
+            res.render('create', {
+                logged_in: req.session.logged_in,
+                userId: res.session.user_id,
+            });
+            return;
+        } else {
+            res.redirect('/login');
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+});
+
+router.get('/create/:id', async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ["name"],
+                },
+                {
+                    model: Comment,
+                    include: [User],
+                },
+            ],
+        });
+
+        const blogPost = blogPostData.get({ plain: true });
+        console.log(blogPost);
+
+        if (req.session.logged_in) {
+            res.render('edit', {
+                ...blogPost,
+                logged_in: req.session.logged_in,
+                userId: req.session.user_id,
+            });
+            return;
+        } else {
+            res.redirect('/login');
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.all('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
+});
+
+// export
+module.exports = router;
