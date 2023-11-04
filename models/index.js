ce2a49424a0d961887c33a1a376c4a7331b472f7
Janet Webster <twixmixy@gmail.com>
2023-11-04 11:20:15 -0400

    Created the relationship model index

---
+++ models/index.js
@@ -0,0 +1,35 @@
+// imports
+const User = require('./User');
+const BlogPost = require('./BlogPost');
+const Comment = require('./Comment');
+
+// relationships between model tables
+User.hasMany(BlogPost, {
+    foreignKey: "user_id",
+    onDelete: 'CASCADE',
+});
+
+BlogPost.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+Comment.belongsTo(BlogPost, {
+    foreignKey: "blogPost_id",
+    onDelete: 'CASCADE',
+});
+
+BlogPost.hasMany(Comment, {
+    foreignKey: "blogPost_id",
+    onDelete: 'CASCADE',
+});
+
+module.exports = { User, BlogPost, Comment };
