3e442ba147deb6dac69efeb731c926acc741d489
Janet Webster <twixmixy@gmail.com>
2023-11-04 20:56:07 -0400

    Fixed the login issue with AskBCS help

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { email: req.body } });
+        const userData = await User.findOne({ where: { email: req.body.email } });
 
         if (!userData) {
             console.log("Could not find that user");
