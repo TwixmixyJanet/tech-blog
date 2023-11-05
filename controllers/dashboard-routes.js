e95311f0a2fe95d54ac2433b072add4bc4ff8def
Janet Webster <twixmixy@gmail.com>
2023-11-05 18:35:50 -0500

    Built dashboard edit by id and new

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -58,3 +58,43 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ["id", "title", "description", "date_created"],
+        include: [
+            {
+                model: User,
+                attributes: ["name"],
+            },
+            {
+                model: Comment,
+                attributes: ["id", "comment_body", "date_created", "user_id", "blogPost_id"],
+                include: {
+                    model: User,
+                    attributes: ["name"],
+                },
+            },
+        ],
+    })
+    .then((postData) => {
+        if (!postData) {
+            res.status(404).json({ message: "Could not find posts with this ID"});
+            return;
+        }
+        const post = postData.get({ plain: true });
+        res.render('edit', { post, loggedIn: true });
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/new', (req, res) => {
+    res.render('new');
+});
+
+module.exports = routers;
